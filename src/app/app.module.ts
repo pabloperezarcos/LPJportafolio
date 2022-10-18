334d9a738a2a23888dd934968e8d02af45da7d5c
pabloperezarcos <pabloperezarcos@gmail.com>
2022-10-18 18:40:46 -0300

    modif. home alumno

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-//import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-//import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
