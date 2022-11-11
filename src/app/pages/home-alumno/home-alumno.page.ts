87da675e70445a508bc3034c4ba094f331130689
pabloperezarcos <pabloperezarcos@gmail.com>
2022-11-11 20:29:35 -0300

    BD para registro asistencia OK.

--- src/app/pages/home-alumno/home-alumno.page.ts
+++ src/app/pages/home-alumno/home-alumno.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-//import { BarcodeScannerPlugin } from '@capacitor-community/barcode-scanner';
+import { RegistroasistenciaService } from 'src/app/services/registroasistencia.service';
 
 @Component({
   selector: 'app-home-alumno',
@@ -15,8 +15,27 @@ export class HomeAlumnoPage implements OnDestroy {
   scanResult: any;
   content_visibility = '';
 
-  constructor() {
-    //this.content_visibility = 'show';
+  bd_asistencia: any = [];
+  registro: string = "";
+  id: number = 0;
+
+  constructor(
+    public registroAsistencia: RegistroasistenciaService
+  ) {
+    this.registroAsistencia.crearBaseDatos().then(() => {
+      this.getAsistencia();
+    });
+  }
+
+  getAsistencia() {
+    this.registroAsistencia.getAsistencia().then((data) => {
+      this.bd_asistencia = [];
+      if (data.rows.length > 0) {
+        for (var i = 0; i < data.rows.length; i++) {
+          this.bd_asistencia.push(data.rows.item(i));
+        }
+      }
+    });
   }
 
   ngOnDestroy(): void {
@@ -52,11 +71,24 @@ export class HomeAlumnoPage implements OnDestroy {
       if (result?.hasContent) {
         this.scanResult = result.content;
         console.log(this.scanResult);
+
+        this.agregarAsistencia(this.scanResult);
       }
     } catch (e) {
       console.log(e);
       this.stopScan();
     }
+  }
+
+  agregarAsistencia(scanResult) {
+    this.registro = scanResult.nombre;
+
+    this.registroAsistencia.addAsistencia(this.registro)
+      .then((registro) => {
+        this.registro = "";
+        alert(registro);
+        this.getAsistencia();
+      });
   }
 
   stopScan() {
